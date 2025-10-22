1917142bae91dc0ce8cb8410ed11f4ab68240abf
YoelUb <yurqubar@myuax.com>
2025-10-22 20:14:26 +0200

    Done, only left stmp with gmail

--- src/umbrella/db/models.py
+++ src/umbrella/db/models.py
@@ -1,20 +1,20 @@
 import datetime
 from typing import Optional
 from zoneinfo import ZoneInfo
+
 from sqlmodel import SQLModel, Field
 
 
 def get_madrid_time() -> datetime.datetime:
+    madrid_time_aware = datetime.datetime.now(ZoneInfo("Europe/Madrid"))
 
-    return datetime.datetime.now(ZoneInfo("Europe/Madrid"))
+    return madrid_time_aware.replace(tzinfo=None)
 
 
 class Evento(SQLModel, table=True):
-
-
     id: Optional[int] = Field(default=None, primary_key=True)
 
-    event_id:str = Field(index=True)
+    event_id: str = Field(index=True)
 
     tipo_dato: str
 
@@ -23,4 +23,3 @@ class Evento(SQLModel, table=True):
     status: str
 
     created_at: datetime.datetime = Field(default_factory=get_madrid_time, nullable=False)
-
